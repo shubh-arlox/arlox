c513d7ba033df7d3c9c4df26ae4568f4a4a4a62e
shubh <shubhamtiwari9610518@gmail.com>
2025-12-22 20:56:02 +0530

    case

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,8 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -15,6 +16,8 @@ export default function RootLayout({ children }) {
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
       </head>
       <body className="min-h-screen flex flex-col font-sans">
+        <SpeedInsights/>
+        <Analytics/>
         <Navbar />
         <main className="flex-1 w-full">{children}</main>
         <Footer />
